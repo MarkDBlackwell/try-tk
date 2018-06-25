df913aa883a91d3b5ea8a092c52c64b70dac51c4
Mark D. Blackwell <markdblackwell01@gmail.com>
2018-06-25 09:54:58 -0400

    pack doesn't take a block well

--- tk_example.rb
+++ tk_example.rb
@@ -4,6 +4,8 @@
 Author: Mark D. Blackwell (google me)
 mdb June 25, 2018 - created
 
+Note: method pack doesn't take a block well.
+
 See:
 Infinite Skills: Ruby Programming 65 - Introduction To Tk - Bui Kenny
 https://youtu.be/8YbqtLRnrFs
@@ -17,11 +19,7 @@ end
 
 TkLabel.new root do
   text 'A sample GUI using Tk'
-  pack do
-    padx 15
-    pady 15
-    side 'left'
-  end
+  pack padx: 15, pady: 15, side: :left
 end
 
 Tk.mainloop
